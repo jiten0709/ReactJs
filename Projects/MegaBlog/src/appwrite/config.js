d4bb9b477e66d995efdf768f906c2ff86e771d46
jiten0709 <parmar.jiten.microsoft@gmail.com>
2024-06-26 20:47:55 +0530

    MegaBlog progress

---
+++ Projects/MegaBlog/src/appwrite/config.js
@@ -0,0 +1,11 @@
+import conf from "../conf/conf";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+
+export class Service {
+    
+}
+
+const service = new Service()
+
+export default service
